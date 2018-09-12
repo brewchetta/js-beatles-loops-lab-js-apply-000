010de4f901492b9f69f662fe8381badbd14f174e
Chett Tiller <chett.tiller@gmail.com>
2018-09-12 16:52:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
-var the BeatlesPlay = function(musicians,instruments) {
+var theBeatlesPlay = function(musicians,instruments) {
     var strings = [];
     for(i=0,i<musicians.length,i++) {
       strings.push(musicians[i] + " plays " + instruments[i])
