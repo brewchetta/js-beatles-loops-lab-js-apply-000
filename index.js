ac9e250043c5be49be25328e38735a76816f83bc
Chett Tiller <chett.tiller@gmail.com>
2018-09-12 16:57:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ var theBeatlesPlay = function(musicians,instruments) {
 };
 
 var johnLennonFacts = function() {
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian","He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  
+
 };
